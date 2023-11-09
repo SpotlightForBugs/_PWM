d5f3304d5651a8e0e4e170a1c9f65ddf100c011d
Johannes Häusler <mail@spotlightforbugs.eu>
2023-11-09 09:12:51 +0000

    init 2

---
+++ Account.java
@@ -0,0 +1,14 @@
+//Account part of the password manager
+// Last modified: 9.11.2023
+
+/**
+ * 
+ * 
+ */
+public class Account {
+    //master password for the account
+    private String masterPassword;
+    //account name
+    private String accountName;
+}
+    
